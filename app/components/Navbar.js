75137295dd59d0bd0fc20d036702d8fe132ed232
vikramcse <vikramcse.10@gmail.com>
2016-02-08 02:20:23 +0530

    Navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,36 @@
+var React = require('react');
+
+var Navbar = React.createClass({
+  render() {
+    return (
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+            aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">My Shop</a>
+        </div>
+        <div id="navbar" className="navbar-collapse collapse">
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a href="#">Store</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    );
+  }
+});
+
+module.exports = Navbar;
