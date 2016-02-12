bc8af979431b32c35634dec90e83e82e6b624f56
vikramcse <vikramcse.10@gmail.com>
2016-02-12 12:59:13 +0530

    Connected front store to firebase

--- app/components/App.js
+++ app/components/App.js
@@ -2,13 +2,34 @@ var React = require('react');
 var Navbar = require('./Navbar');
 var Catalog = require('./Catalog');
 
+var ref = require('../constants').firebase_ref;
+var Firebase = require('firebase');
+var ReactFire = require('reactfire');
+
 var App = React.createClass({
+  mixins: [ReactFire],
+  getInitialState: function() {
+    return {
+      catalog: {},
+      loaded: false
+    };
+  },
+  componentWillMount: function() {
+    var fb = new Firebase(ref + 'catalog/');
+    this.bindAsObject(fb, 'catalog');
+    fb.on('value', this.handleProductsLoaded);
+  },
+  handleProductsLoaded: function() {
+    this.setState({
+      loaded: true
+    });
+  },
   render() {
     return (
       <div>
         <Navbar />
-        <div className="container">
-          <Catalog />
+        <div className={"container content " + (this.state.loaded ? 'loaded' : '')}>
+          <Catalog catalog={this.state.catalog}/>
         </div>
       </div>
     );
--- app/components/Catalog.js
+++ app/components/Catalog.js
@@ -2,50 +2,30 @@ var React = require('react');
 var CatalogItem = require('./CatalogItem');
 
 var Catalog = React.createClass({
+  renderItems: function() {
+    if (this.props.catalog && Object.keys(this.props.catalog).length === 0) {
+      return <h4>No items in store</h4>
+    } else {
+      var children = [];
+      for (var k in this.props.catalog) {
+        if (typeof this.props.catalog[k] === 'object') {
+          children.push (
+            <CatalogItem
+              key={k}
+              title={this.props.catalog[k].title}
+              img={this.props.catalog[k].url}
+              summary={this.props.catalog[k].summary}
+              cost={this.props.catalog[k].cost}/>
+          );
+        }
+      }
+      return children;
+    }
+  },
   render() {
     return (
       <div className="row stylish-panel">
-        <CatalogItem
-          title="Shoes"
-          img="http://lorempixel.com/200/200/abstract/1/"
-          summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eligendi."
-          cost="100"
-          qty="50"/>
-
-          <CatalogItem
-            title="Shoes"
-            img="http://lorempixel.com/200/200/abstract/1/"
-            summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eligendi."
-            cost="100"
-            qty="50"/>
-
-            <CatalogItem
-              title="Shoes"
-              img="http://lorempixel.com/200/200/abstract/1/"
-              summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eligendi."
-              cost="100"
-              qty="50"/>
-
-              <CatalogItem
-                title="Shoes"
-                img="http://lorempixel.com/200/200/abstract/1/"
-                summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eligendi."
-                cost="100"
-                qty="50"/>
-
-                <CatalogItem
-                  title="Shoes"
-                  img="http://lorempixel.com/200/200/abstract/1/"
-                  summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eligendi."
-                  cost="100"
-                  qty="50"/>
-
-                  <CatalogItem
-                    title="Shoes"
-                    img="http://lorempixel.com/200/200/abstract/1/"
-                    summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eligendi."
-                    cost="100"
-                    qty="50"/>
+        {this.renderItems()}
       </div>
     );
   }
